b73620179c05dbd7123080393b1d754a64970763
Ramilsons <ramilsonfelixsilva9@gmail.com>
2024-03-23 12:06:18 -0300

    feat(section horizontal product card): add jsx sctruture

---
+++ components/horizontalProductCard/horizontalProductCard.tsx
@@ -0,0 +1,29 @@
+import type { ImageWidget } from "apps/admin/widgets.ts";
+
+import Image from "apps/website/components/Image.tsx";
+
+export interface Props {
+    image: ImageWidget,
+    title: string,
+    description: string,
+    price: string
+}
+
+export default function HorizontalProductCard({ image, title, description, price }: Props) {
+    return (
+        <div>
+            <div>
+                <Image height={120} width={120} src={image} />
+            </div>
+
+            <div>
+                <h2>{title}</h2>
+                <h3>{description}</h3>
+
+                <h5>{price}</h5>
+            </div>
+
+            <button type="button">{'Adicionar ao Carrinho'}</button>
+        </div>
+    )
+}
